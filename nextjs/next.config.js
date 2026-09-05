448c38374b1c2818bc6e9480b144507012707550
daksh-prajapat <prajapatlokesh08112003@gmail.com>
2026-09-05 12:27:47 +0530

    Fix next.config.js - remove turbopack config

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  turbopack: {
-    root: __dirname,
-  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.join(__dirname, 'src')
     return config

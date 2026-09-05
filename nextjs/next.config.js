b853394f90197d9fddf351fb576ecf6cc2191758
daksh-prajapat <prajapatlokesh08112003@gmail.com>
2026-09-05 12:30:02 +0530

    Remove unoptimized images config causing static export confusion

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -2,9 +2,6 @@
 const path = require('path')
 
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.join(__dirname, 'src')
     return config

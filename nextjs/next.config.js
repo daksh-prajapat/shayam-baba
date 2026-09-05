0bd42881d21eb9bac04632b0c61da63a16e265b6
daksh-prajapat <prajapatlokesh08112003@gmail.com>
2026-09-05 12:32:00 +0530

    Fix next.config.js - remove webpack alias, use turbopack

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
-
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.join(__dirname, 'src')
-    return config
-  },
+  turbopack: {},
 }
 
 module.exports = nextConfig
